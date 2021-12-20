8899703c39601d3207149ad27d132fb8289cf48b
jessicacorreaes <jessicacorreaes@gmail.com>
2021-12-20 20:34:52 -0300

    organizando

---
+++ src/main/java/com/curso/nelioalves/NelioalvesApplication.java
@@ -0,0 +1,13 @@
+package com.curso.nelioalves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NelioalvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NelioalvesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/nelioalves/NelioalvesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.nelioalves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NelioalvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
