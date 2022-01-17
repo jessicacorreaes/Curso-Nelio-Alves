661eb7613aea42100dacf6fa78d0b258b1be7088
jessicacorreaes <jessicacorreaes@gmail.com>
2022-01-17 00:37:12 -0300

    Exercicios secao 13

---
+++ src/main/java/com/curso/nelioalves/secao13/application/Program.java
@@ -0,0 +1,66 @@
+package com.curso.nelioalves.secao13.application;
+
+import com.curso.nelioalves.secao13.entities.Client;
+import com.curso.nelioalves.secao13.entities.Order;
+import com.curso.nelioalves.secao13.entities.Orderltem;
+import com.curso.nelioalves.secao13.entities.Product;
+import com.curso.nelioalves.secao13.enuns.OrderStatus;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.println("Enter client data:");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.next();
+        System.out.print("Birth date (DD/MM/YYYY): ");
+        Date birthDate = sdf.parse(sc.next());
+
+        Client client = new Client(name, email, birthDate);
+
+        System.out.println("Enter order data:");
+        System.out.print("Status: ");
+        OrderStatus status = OrderStatus.valueOf(sc.next());
+
+        Order order = new Order(new Date(), status, client);
+
+        System.out.print("How many items to this order? ");
+        int n = sc.nextInt();
+        for (int i=1; i<=n; i++) {
+            System.out.println("Enter #" + i + " item data:");
+            System.out.print("Product name: ");
+            sc.nextLine();
+            String productName = sc.nextLine();
+            System.out.print("Product price: ");
+            double productPrice = sc.nextDouble();
+
+            Product product = new Product(productName, productPrice);
+
+            System.out.print("Quantity: ");
+            int quantity = sc.nextInt();
+
+            Orderltem orderItem = new Orderltem(quantity, productPrice, product);
+
+            order.addItem(orderItem);
+        }
+
+        System.out.println();
+        System.out.println("ORDER SUMMARY:");
+        System.out.println(order);
+
+        sc.close();
+    }
+
+}
---
+++ src/main/java/com/curso/nelioalves/secao13/enuns/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.curso.nelioalves.secao13.enuns;
+
+public enum OrderStatus {
+
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+
+}
