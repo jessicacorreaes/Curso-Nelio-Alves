5a376a839b3bd7d2e0aa7b9dc8a4b2f4f62cd33e
jessicacorreaes <jessicacorreaes@gmail.com>
2022-01-28 16:49:18 -0300

    Exercicios secao 18

---
+++ src/main/java/com/curso/nelioalves/secao18/services/OnlinePaymentService.java
@@ -0,0 +1,9 @@
+package com.curso.nelioalves.secao18.services;
+
+public interface OnlinePaymentService {
+
+    Double paymentFee(Double amount);
+
+    Double interest(Double amount, Integer months);
+
+}
